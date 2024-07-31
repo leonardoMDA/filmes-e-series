44c2f6857e6538121d5cf8d3154c5e10463cea93
leonardoMDA <162999522+leonardoMDA@users.noreply.github.com>
2024-07-31 12:10:08 -0300

    Add files via upload

--- modelo.py
+++ modelo.py
@@ -11,18 +11,16 @@ class Programa:
     @nome.setter
     def nome (self,novo_nome):
         self._nome = novo_nome.title()
-        
+    
     @property
-    def likes(self):
+    def nome(self):
         return self._likes
-
 
     def dar_likes(self):
         self._likes += 1
-
-    def imprime(self):
-        print(f'{self.nome} - {self.ano} - {self._likes} likes')
-
+    
+    def __str__(self):
+        return f'{self.nome} - {self.ano} - {self._likes} Likes'
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
@@ -31,20 +29,38 @@ class Filme(Programa):
         self._likes = 0
 
 class Serie(Programa):
-     def __init__(self, nome, ano, temporadas, duracao):
+    def __init__(self, nome, ano, temporada):
         super().__init__(nome,ano)
-        self.ano = ano
-        self.temporadas = temporadas
+        self.temporada = temporada
         self._likes = 0
 
-vingadores = Filme('Vingadores - guerra infinita', 2018, 160)
-vingadores.dar_likes()
+class Playlist(list):
+    def __init__(self, nome, programas):
+        self.nome = nome
+        super().__init__(programas)
 
+   
+vingadores = Filme('Vingadores - Guerra Infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('todo mundo em panico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
+
+vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+demolidor.dar_likes()
 
-filmes_e_series = [vingadores,atlanta]
+filmes_e_series = [vingadores,atlanta,demolidor,tmep]
+playlist_fim_de_semana = Playlist('fim de semana' ,filmes_e_series)
 
-for programa in filmes_e_series:
-    programa.imprime()
+print (f'Tamanho da Playlist: {len(playlist_fim_de_semana)}')
+
+for programa in playlist_fim_de_semana:
+    print (programa)
+    
+print (f'Está ou não está? {demolidor in playlist_fim_de_semana}')    
